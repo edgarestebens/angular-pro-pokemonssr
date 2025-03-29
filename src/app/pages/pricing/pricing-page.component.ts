2daccaeb6155764913e7742330ea0d3b8a2a5eba
Edgar Jaramillo R <ejaramillor@icetex.gov.co>
2025-03-29 05:29:39 -0500

    modificaciones ssr

--- src/app/pages/pricing/pricing-page.component.ts
+++ src/app/pages/pricing/pricing-page.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector:'page-princing',
@@ -7,4 +9,26 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './pricing-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PricingPageComponent { }
+export default class PricingPageComponent implements OnInit{
+
+  private title = inject(Title);
+  private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID);
+
+  ngOnInit(): void {
+
+    //para detectar si esta en el servidor o no
+    if(isPlatformBrowser(this.platform)){
+
+      document.title = 'Princing Page';
+
+    }
+
+
+    // this.title.setTitle("Princing Page");
+    // this.meta.updateTag({name: 'description',content: 'Este es mi Princing page'});
+    // this.meta.updateTag({name: 'og:title',content: 'Princing Page'});
+    // this.meta.updateTag({name: 'keywords',content: 'Hola mundo Edgar Jaramillo,curso de angular Pro'});
+
+  }
+}
